4375b25aa0bfe5257b35696790051cd6aefb092a
Jaowzinho <157393407+Jaowzinho@users.noreply.github.com>
2025-05-25 14:26:48 -0300

    Add files via upload

--- src/main/java/com/colecao/colecao_jogos/controller/JogoController.java
+++ src/main/java/com/colecao/colecao_jogos/controller/JogoController.java
@@ -3,6 +3,7 @@ package com.colecao.colecao_jogos.controller;
 import com.colecao.colecao_jogos.model.Jogo;
 import com.colecao.colecao_jogos.repository.JogoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,23 +16,52 @@ public class JogoController {
     @Autowired
     private JogoRepository jogoRepository;
 
+    // Listar todos os jogos
     @GetMapping
     public List<Jogo> listarJogos() {
         return jogoRepository.findAll();
     }
 
+    // Buscar jogo por ID
     @GetMapping("/{id}")
-    public Optional<Jogo> buscarPorId(@PathVariable Integer id) {
-        return jogoRepository.findById(id);
+    public ResponseEntity<Jogo> buscarJogoPorId(@PathVariable Integer id) {
+        Optional<Jogo> jogo = jogoRepository.findById(id);
+        return jogo.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
+    // Adicionar novo jogo
     @PostMapping
-    public Jogo criarJogo(@RequestBody Jogo jogo) {
+    public Jogo adicionarJogo(@RequestBody Jogo jogo) {
         return jogoRepository.save(jogo);
     }
 
+    // Atualizar jogo existente
+    @PutMapping("/{id}")
+    public ResponseEntity<Jogo> atualizarJogo(@PathVariable Integer id, @RequestBody Jogo jogoAtualizado) {
+        Optional<Jogo> optionalJogo = jogoRepository.findById(id);
+
+        if (!optionalJogo.isPresent()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        Jogo jogo = optionalJogo.get();
+        jogo.setNome(jogoAtualizado.getNome());
+        jogo.setData(jogoAtualizado.getData());
+        jogo.setImagem(jogoAtualizado.getImagem());
+
+        jogoRepository.save(jogo);
+
+        return ResponseEntity.ok(jogo);
+    }
+
+    // Deletar jogo por ID
     @DeleteMapping("/{id}")
-    public void deletarJogo(@PathVariable Integer id) {
+    public ResponseEntity<Void> deletarJogo(@PathVariable Integer id) {
+        if (!jogoRepository.existsById(id)) {
+            return ResponseEntity.notFound().build();
+        }
+
         jogoRepository.deleteById(id);
+        return ResponseEntity.noContent().build();
     }
 }
